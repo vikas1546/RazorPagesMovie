499a300e6c99e98566a93468f108b6950f3e75de
vikas <vikas1546@gmail.com>
2019-03-25 16:30:04 +0530

    This is initial commit

---
+++ RazorPagesMovie/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<RazorPagesMovieContext>();
+                    context.Database.Migrate();
+                    // A production app would not call Database.Migrate.
+                    //It's added to the preceding code to prevent the following exception when Update-Database has not been run:
+                    //SqlException: Cannot open database "RazorPagesMovieContext-21" requested by the login. The login failed.Login failed for user 'user name'.
+
+                    //Force the app to initialize (call the methods in the Startup class) so the seed method runs. 
+                    //To force initialization, IIS Express must be stopped and restarted. 
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
